05866bdf96c86b3928ac2ad24eca7de67d363350
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-31 13:48:32 +0530

    Count of digit

--- Basic_Math/ArmstrongNumber.java
+++ Basic_Math/ArmstrongNumber.java
@@ -1,17 +1,18 @@
 package Basic_Math;
-
+// Armstrong Number means a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+// This program checks if a number is an Armstrong number or not.
 public class ArmstrongNumber {
   public static void main(String[] args) {
     int number = 153;
     int originalNumber = number; // Store the original number to compare later
     int sum = 0;
 
-    while (number > 0) {
-      int remainder = number % 10;
-      sum += remainder * remainder * remainder;
-      number = number / 10;
+    while (number > 0) { // Loop to extract each digit
+      int remainder = number % 10; // Get the last digit
+      sum += remainder * remainder * remainder; // Cube the digit and add to sum
+      number = number / 10; // Remove the last digit
     }
-
+ // Check if the sum of cubes of digits is equal to the original number
     if (originalNumber == sum) {
       System.out.println(originalNumber + " is an Armstrong number.");
     } else {
