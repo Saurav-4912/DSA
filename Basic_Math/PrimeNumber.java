dada81a09182606fa335272ac01069d6274d2b15
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-31 09:35:12 +0530

    Added Comment in code

--- Basic_Math/PrimeNumber.java
+++ Basic_Math/PrimeNumber.java
@@ -1,24 +1,25 @@
 package Basic_Math;
-
+// Prime Number means a number that is greater than 1 and has no positive divisors other than 1 and itself.
+// This program checks if a number is prime or not.
 class PrimeNumber{
   public static void main(String[] args) {
     int number = 28;
-    boolean isPrime = true;
+    boolean isPrime = true;  // Let's assume the number is prime number
 
-    if(number <= 1){
+    if(number <= 1){   // Number less than or equal to 1 are not prime number
       isPrime = false;
-    }else{
+    }else{              // if number is greater than 1, we check for divisibility
       for( int i =2; i<= Math.sqrt(number); i++){
         if(number % i == 0){
-          isPrime = false;
+          isPrime = false;  // If number is divisible by any number other than 1 and itself, it is not prime
           break;
         }
       }
     }
 
-    if(isPrime){
+    if(isPrime){ // If isPrime is still true, then the number is prime
       System.out.println(number + " is a prime number, ");
-    }else{
+    }else{    // If isPrime is false, then the number is not prime
       System.out.println(number + " is not a prime number, ");
     }
 
