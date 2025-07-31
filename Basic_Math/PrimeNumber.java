45ba05af774fd2983cd761a3a051689ae353c2b8
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-31 09:21:20 +0530

    Check given number is prime or not

--- Basic_Math/PrimeNumber.java
+++ Basic_Math/PrimeNumber.java
@@ -2,7 +2,26 @@ package Basic_Math;
 
 class PrimeNumber{
   public static void main(String[] args) {
-    int primeNumber = 28;
-    
+    int number = 28;
+    boolean isPrime = true;
+
+    if(number <= 1){
+      isPrime = false;
+    }else{
+      for( int i =2; i<= Math.sqrt(number); i++){
+        if(number % i == 0){
+          isPrime = false;
+          break;
+        }
+      }
+    }
+
+    if(isPrime){
+      System.out.println(number + " is a prime number, ");
+    }else{
+      System.out.println(number + " is not a prime number, ");
+    }
+
+
   }
 }
